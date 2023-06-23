92cd61a04532cbb312887cdd7212d5ceb9182863
FrogTesseract <118939591+FrogTesseract@users.noreply.github.com>
2023-06-23 21:32:02 +0100

    6_doubles_letter.py more efficient
    Completed an effective challenge 6, learning about any() and zip() along the way

--- 6_double_letters.py
+++ 6_double_letters.py
@@ -1,12 +1,3 @@
-# def double_letters(string):
-#     print(string[0])
-#     previous = string[0]
-#     for i in range(1,len(string)):
-#         if string[i] == previous:
-#             return True
-#         previous = string[i]
-#     return False
-
 def double_letters(string):
     for i in range(0,len(string)-1):
         if string[i] == string[i+1]:
@@ -15,3 +6,9 @@ def double_letters(string):
 
 print(double_letters("hello"))
 
+
+def efficient_double_letters(string): #Thanks for the help explaining this!
+    return any([a == b for a, b in zip(string, string[1:])])#zip goes through the strings adding each letter to a tuple, this is then compared if they are both
+# the same letter and adding to a list of booleans values, any then checks if any are true
+
+print(efficient_double_letters("hello"))
