435cdd6f7eb2cafe9c26c475d31b165fd4f2b77a
FrogTesseract <118939591+FrogTesseract@users.noreply.github.com>
2023-06-23 16:58:12 +0100

    Create 6_double_letters.py
    Completed challenge 6

---
+++ 6_double_letters.py
@@ -0,0 +1,15 @@
+# def double_letters(string):
+#     print(string[0])
+#     previous = string[0]
+#     for i in range(1,len(string)):
+#         if string[i] == previous:
+#             return True
+#         previous = string[i]
+#     return False
+
+def double_letters(string):
+    for i in range(0,len(string)-1):
+        if string[i] == string[i+1]:
+            return True
+    return False
+print(double_letters("hello"))
